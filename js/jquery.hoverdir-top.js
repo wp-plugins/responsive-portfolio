12e9a8f6aa20111d463bd9e4ee3f7da728a5e6f8
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-22 09:31:05 +0000

    commit version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/responsive-portfolio/tags/1.5@1051397 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jquery.hoverdir-top.js
@@ -0,0 +1,211 @@
+
+;( function( $, window, undefined ) {
+	
+	'use strict';
+
+	$.HoverDir_Top = function( options, element ) {
+		
+		this.$el = $( element );
+		this._init( options );
+
+	};
+
+	// the options
+	$.HoverDir_Top.defaults = {
+		speed : 300,
+		easing : 'ease',
+		hoverDelay : 0,
+		inverse : false
+	};
+
+	$.HoverDir_Top.prototype = {
+
+		_init : function( options ) {
+			
+			// options
+			this.options = $.extend( true, {}, $.HoverDir_Top.defaults, options );
+			// transition properties
+			this.transitionProp = 'all ' + this.options.speed + 'ms ' + this.options.easing;
+			// support for CSS transitions
+			this.support = Modernizr.csstransitions;
+			// load the events
+			this._loadEvents();
+
+		},
+		_loadEvents : function() {
+
+			var self = this;
+			
+			this.$el.on( 'mouseenter.hoverdir_top, mouseleave.hoverdir_top', function( event ) {
+				
+				var $el = $( this ),
+					$hoverElem = $el.find( 'div' ),
+					direction = self._getDir( $el, { x : event.pageX, y : event.pageY } ),
+					styleCSS = self._getStyle( direction );
+				
+				if( event.type === 'mouseenter' ) {
+					
+					$hoverElem.hide().css( styleCSS.from );
+					clearTimeout( self.tmhover );
+
+					self.tmhover = setTimeout( function() {
+						
+						$hoverElem.show( 0, function() {
+							
+							var $el = $( this );
+							if( self.support ) {
+								$el.css( 'transition', self.transitionProp );
+							}
+							self._applyAnimation( $el, styleCSS.to, self.options.speed );
+
+						} );
+						
+					
+					}, self.options.hoverDelay );
+					
+				}
+				else {
+				
+					if( self.support ) {
+						$hoverElem.css( 'transition', self.transitionProp );
+					}
+					clearTimeout( self.tmhover );
+					self._applyAnimation( $hoverElem, styleCSS.from, self.options.speed );
+					
+				}
+					
+			} );
+
+		},
+		// credits : http://stackoverflow.com/a/3647634
+		_getDir : function( $el, coordinates ) {
+			
+			// the width and height of the current div
+			var w = $el.width(),
+				h = $el.height(),
+
+				// calculate the x and y to get an angle to the center of the div from that x and y.
+				// gets the x value relative to the center of the DIV and "normalize" it
+				x = ( coordinates.x - $el.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
+				y = ( coordinates.y - $el.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
+			
+				// the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);
+				// first calculate the angle of the point,
+				// add 180 deg to get rid of the negative values
+				// divide by 90 to get the quadrant
+				// add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left) **/
+				direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 ) % 4;
+			
+			return direction;
+			
+		},
+		_getStyle : function( direction ) {
+			
+			var fromStyle, toStyle,
+				slideFromTop = { left : '0px', top : '-100%' },
+				slideFromBottom = { left : '0px', top : '100%' },
+				slideFromLeft = { left : '-100%', top : '0px' },
+				slideFromRight = { left : '100%', top : '0px' },
+				slideTop = { top : '0px' },
+				slideLeft = { left : '0px' };
+			
+			switch( direction ) {
+				case 0:
+					// from top
+					fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
+					toStyle = slideTop;
+					break;
+				case 1:
+					// from top
+					fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
+					toStyle = slideTop;
+					break;
+				case 2:
+					// from top
+					fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
+					toStyle = slideTop;
+					break;
+				case 3:
+					// from top
+					fromStyle = !this.options.inverse ? slideFromTop : slideFromBottom;
+					toStyle = slideTop;
+					break;
+			};
+			
+			return { from : fromStyle, to : toStyle };
+					
+		},
+		// apply a transition or fallback to jquery animate based on Modernizr.csstransitions support
+		_applyAnimation : function( el, styleCSS, speed ) {
+
+			$.fn.applyStyle = this.support ? $.fn.css : $.fn.animate;
+			el.stop().applyStyle( styleCSS, $.extend( true, [], { duration : speed + 'ms' } ) );
+
+		},
+
+	};
+	
+	var logError = function( message ) {
+
+		if ( window.console ) {
+
+			window.console.error( message );
+		
+		}
+
+	};
+	
+	$.fn.hoverdir_top = function( options ) {
+
+		var instance = $.data( this, 'hoverdir_top' );
+		
+		if ( typeof options === 'string' ) {
+			
+			var args = Array.prototype.slice.call( arguments, 1 );
+			
+			this.each(function() {
+			
+				if ( !instance ) {
+
+					logError( "cannot call methods on hoverdir_top prior to initialization; " +
+					"attempted to call method '" + options + "'" );
+					return;
+				
+				}
+				
+				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
+
+					logError( "no such method '" + options + "' for hoverdir_top instance" );
+					return;
+				
+				}
+				
+				instance[ options ].apply( instance, args );
+			
+			});
+		
+		} 
+		else {
+		
+			this.each(function() {
+				
+				if ( instance ) {
+
+					instance._init();
+				
+				}
+				else {
+
+					instance = $.data( this, 'hoverdir_top', new $.HoverDir_Top( options, this ) );
+				
+				}
+
+			});
+		
+		}
+		
+		return instance;
+		
+	};
+	
+} )( jQuery, window );
