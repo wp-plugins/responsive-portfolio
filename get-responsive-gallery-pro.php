89f7568abe80c9e847c1e9d9ca648e182f35c338
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 05:16:29 +0000

    commit version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/responsive-portfolio/tags/2.0@1141684 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ get-responsive-gallery-pro.php
@@ -0,0 +1,177 @@
+<div class="row-fluid pricing-table pricing-three-column" style="margin-top: 10px;display:block;width:100%;overflow:hidden">
+
+	<div class="purchase_btn_div" style="margin-top:20px;text-align: center;">
+	  <a  style= "margin-right:10px;" href="http://demo.weblizar.com/responsive-portfolio-pro/" target="_new" class="btn btn-primary btn-lg">View Demo</a>
+	  <a style= "margin-right:10px;" href="http://weblizar.com/plugins/responsive-portfolio-pro/" target="_new" class="btn btn-success btn-lg">Try Before Buy</a>
+	  <a href="http://weblizar.com/plugins/responsive-portfolio-pro/" target="_new" class="btn btn-danger btn-lg">Upgrade To Pro</a>
+		
+	</div>
+	
+	<div class="plan-name" style="margin-top:40px;text-align: center;">
+        <h2 style="font-weight: bold;font-size: 36px;padding-top: 30px;padding-bottom: 10px;">Responsive Portfolio Pro Screenshots</h2>
+		<h6 style="font-size: 22px;padding-top: 10px;padding-bottom: 10px;">A Perfect Responsive Portfolio Plugin for display content like images, vimeo/youtube video, Albums etc.</h6>
+    </div>
+	<div class="plan-name"><!-- SLIDER-INTRO-->
+				<!--===============================================================-->
+			<div class="col-md-12 col-sm-12">
+				<a href="http://weblizar.com/plugins/responsive-portfolio-pro/" target="_new"><h2 style="font-weight: bold;font-size: 26px;padding-top: 20px;padding-bottom: 20px; text-align:center">Responsive Portfolio Pro Demos</h2>
+				<img class="img-responsive" style="width:100%;border: 1px solid #e3e3e3;background: #f7f7f7;padding:10px" src="<?php echo  WEBLIZAR_RP_PLUGIN_URL.'images/portfolio-pro-detial.png'; ?>" alt=""/>
+				</a>
+			</div>
+			
+		</div>
+					
+</div>
+<div class="plan-name" style="padding-top:40px; text-align: center;display:block;width:100%;overflow:hidden">
+ 
+	<div class="purchase_btn_div" style="margin-top:20px;text-align: center;">
+	  <a  style= "margin-right:10px;" href="http://demo.weblizar.com/responsive-photo-gallery-pro/" target="_new" class="btn btn-primary btn-lg">View Demo</a>
+	  <a style= "margin-right:10px;" href="http://demo.weblizar.com/responsive-photo-gallery-admin-demo/" target="_new" class="btn btn-success btn-lg">Try Before Buy</a>
+	  <a href="http://demo.weblizar.com/responsive-photo-gallery-admin-demo/" target="_new" class="btn btn-danger btn-lg">Upgrade To Pro</a>
+		
+	</div>
+</div>
+<div class="plan-name" style="padding-top:40px; text-align: center;display:block;width:100%;overflow:hidden">
+        <h2 style="font-weight: bold;font-size: 36px;padding-top: 10px;padding-bottom: 10px;">Responsive Photo gallery Pro And Free Comparison Table</h2>
+</div>
+<div class="row-fluid pricing-table pricing-three-column" style="margin-top: 10px;">
+	<div class="col-md-4">
+		<div class=" plan ap">
+			<div class="plan-name">
+				<h2>Responsive Portfolio</h2>
+				<span> Pro Feature Set</span>
+			</div>
+			<ul>
+				<li class="plan-feature">Image Hover Animation</li>
+				<li class="plan-feature">Gallery Layout</li>
+				<li class="plan-feature">Hover Color</li>
+				<li class="plan-feature">Hover Color Opacity</li>
+				<li class="plan-feature">Caption Font Style</li>
+				<li class="plan-feature">Light Box</li>
+				<li class="plan-feature">All Gallery Shortcode</li>
+				<li class="plan-feature">Single Gallery Shortcode</li>
+				<li class="plan-feature">Number of Hover Animation</li>
+				<li class="plan-feature">Number of Gallery Layout</li>
+				<li class="plan-feature">Number of Hover Color</li>
+				<li class="plan-feature">Number of Font Style</li>
+				<li class="plan-feature">Number of Lightbox Styles</li>
+				<li class="plan-feature">Drag and Drop image Position</li>
+				<li class="plan-feature">Multiple Image uploader</li>
+				<li class="plan-feature">Shortcode Button on post or page</li>
+				<li class="plan-feature">Unique settings for each gallery</li>
+				<li class="plan-feature">Hide/Show gallery Title and label</li>
+				<li class="plan-feature">Font icon Customization</li>
+				<li class="plan-feature">Google Fonts</li>
+				<li class="plan-feature">Isotope Effects</li>
+				<li class="plan-feature">Video Gallery</li>
+				<li class="plan-feature">Carousel Slider</li>
+				<li class="plan-feature">Album View</li>
+				<li class="plan-feature">External Link</li>
+								 
+			</ul>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class=" plan bp">
+			<div class="plan-name">
+				<h2>Free</h2>
+				<span>$0</span>
+			</div>
+			<ul>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature">1</li>
+				<li class="plan-feature">2</li>
+				<li class="plan-feature">2</li>
+				<li class="plan-feature">10+</li>
+				<li class="plan-feature">0</li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+				<li class="plan-feature">
+					<a  href="#" class="button button-primary button-hero" ><i class="fa fa-download"></i> Download</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+		<div class=" plan cp">
+			<div class="plan-name">
+				<h2>Pro</h2>
+				<span>$19</span>
+			</div>
+			<ul>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature">8</li>
+				<li class="plan-feature">5</li>
+				<li class="plan-feature">Unlimited</li>
+				<li class="plan-feature">500+</li>
+				<li class="plan-feature">4</li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+				<li class="plan-feature">
+					<a class="btn btn-primary btn-lg" target="_new" href="http://demo.weblizar.com/responsive-portfolio-pro/" target="_new" ><i class="fa fa-check-circle"></i> Demo</a>
+					<a class="btn btn-danger btn-lg" target="_new" href="http://weblizar.com/plugins/responsive-portfolio-pro/"  ><i class="fa fa-shopping-cart"></i> Buy</a>
+				</li>
+			</ul>
+		</div>
+	</div>	
+</div>
+<style>
+    .pricing-table .plan ul li.plan-feature {
+        padding: 8px !important;
+    }
+    .ap .plan-name {
+        background-color: #1E8CBE !important;
+    }
+    .bp .plan-name {
+        background-color: #1E8CBE !important;
+    }
+    .cp .plan-name {
+        background-color: #1E8CBE !important;
+    }
+    li {
+        font-size: larger !important;
+    }
+
+    .row-fluid .span4 {
+        width: 30.624% !important;
+    }
+	
+	ul li img {
+		
+	}
+</style>
